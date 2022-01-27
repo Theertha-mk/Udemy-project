3e46db8f17dbd68bd6fd6b5fb9424fe39b3fcc8b
theertha <theertha@bititude.com>
2022-01-27 09:31:42 +0530

    passing ingredients from recipe to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { Recipe } from '../recipe-model'
+import { RecipeServices } from '../recipes.service'
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,8 +8,11 @@ import { Recipe } from '../recipe-model'
   styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe:Recipe;
-  constructor() {}
+  @Input() recipe: Recipe
+  constructor(private recipeService: RecipeServices) {}
 
   ngOnInit(): void {}
+  onAddToShoppingList() {
+    this.recipeService.addingIngredientsToShoppingList(this.recipe.ingredients)
+  }
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,22 +1,30 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core'
+import { Ingredient } from '../shared/ingredient.model'
+import { ShoppingListService } from '../shopping-list/shopping-list.service'
 import { Recipe } from './recipe-model'
-
+@Injectable()
 export class RecipeServices {
-recipeSelected= new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter<Recipe>()
 
   private recipes: Recipe[] = [
     new Recipe(
-      'a test recipe',
+      'Tasty schnitzel',
       'this is just for the test',
       'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      [new Ingredient('meat', 1), new Ingredient('French Fries', 14)],
     ),
     new Recipe(
-      'a test recipe',
+      'Big fat Burger',
       'this is just for the test',
       'https://images.unsplash.com/photo-1542010589005-d1eacc3918f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+      [new Ingredient('buns', 16), new Ingredient('French Fries', 14)],
     ),
   ]
+  constructor(private slService: ShoppingListService) {}
   getRecipes() {
-    return this.recipes.slice();
+    return this.recipes.slice()
+  }
+  addingIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients)
   }
 }
