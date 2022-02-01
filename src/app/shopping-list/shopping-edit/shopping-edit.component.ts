c45e292f5bc230c4283cb300a423d0711ace4ff2
theertha <theertha@bititude.com>
2022-02-01 17:04:11 +0530

    reset,clear,delete shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -31,7 +31,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
       },
     )
   }
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value
     const newIngredient = new Ingredient(value.name, value.amount)
     if (this.editMode) {
@@ -39,6 +39,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient)
     }
+    this.editMode = false
+    form.reset()
+  }
+  onClear() {
+    this.slForm.reset()
+    this.editMode = false
+  }
+  onDelete() {
+    this.slService.deleteIngredient(this.editedItemIndex)
+    this.onClear()
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe()
