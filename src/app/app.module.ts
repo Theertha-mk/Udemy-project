d8eb678342300c4ccdc89d0dac0ce1044842038b
theertha <theertha@bititude.com>
2022-02-09 09:41:03 +0530

    lazy loading  added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,9 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
-import { RecipesModule } from './recipes/recipes.module'
-import { ShoppingListModule } from './shopping-list/shopping-list.module'
 import { SharedModule } from './shared/shared.module'
 import { CoreModule } from './core.module'
-import { AuthModule } from './auth/auth.module'
+
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -16,9 +14,6 @@ import { AuthModule } from './auth/auth.module'
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule,
-    ShoppingListModule,
-    AuthModule,
     SharedModule,
     CoreModule,
   ],
